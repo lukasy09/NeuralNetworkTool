21628494a185e4fe7360ff024049bd05c774f0bf
Łukasz <lukasy09@gmail.com>
2019-09-18 20:39:05 +0200

    some tests with translate

--- src/components/CreatorView/ExpandingCanvas/ExpandingCanvas.js
+++ src/components/CreatorView/ExpandingCanvas/ExpandingCanvas.js
@@ -7,11 +7,30 @@ export default class ExpandingCanvas extends React.Component {
     constructor(props){
         super(props);
     }
-    render(){
 
+    componentDidMount(){
+        let c = this.expandingCanvas;
+        let ctx = c.getContext("2d");
+        ctx.fillRect(10, 10, 100, 50);
+        ctx.fillRect(700, 70, 100, 50);
+        console.log("XD shit");
+    }
+
+    testTranslate = () =>{
+        let ctx = this.expandingCanvas.getContext("2d");
+        let prev = ctx.getImageData(0,0, ctx.canvas.width, ctx.canvas.height);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.translate(100, 0);
+        ctx.putImageData(prev, 10, 0);
+        console.log("no eksde")
+    };
+    render(){
         const {x, y} = this.props.sizeFactors;
         return(
             <div className = {"ExpandingCanvasContainer"}>
+                <div onClick={this.testTranslate}>
+                    TEST translate
+                </div>
                 <canvas id={this.canvasId}
                         className={"ExpandingCanvas"}
                         width={Math.round(x * window.innerWidth)}
@@ -20,4 +39,5 @@ export default class ExpandingCanvas extends React.Component {
             </div>
         )
     }
+
 }
